9c440819fcc51f07c51bb39347c688d134172d2c
Francisco <franciscojanermudoy@gmail.com>
2023-05-30 23:41:29 +0200

    Pagina cocinero avanazada

--- lib/screen/chef_screen.dart
+++ lib/screen/chef_screen.dart
@@ -1,25 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:hostalapp/models/models.dart';
 import 'package:hostalapp/providers/providers.dart';
-import 'package:hostalapp/service/firebase_service.dart';
 import 'package:provider/provider.dart';
 
-class ChefScreen extends StatefulWidget {
+class ChefScreen extends StatelessWidget {
   const ChefScreen({Key? key}) : super(key: key);
 
   @override
-  _ChefScreenState createState() => _ChefScreenState();
-}
-
-class _ChefScreenState extends State<ChefScreen> {
-  late List<Comanda> comandas;
-
-  @override
   Widget build(BuildContext context) {
-    TextEditingController _nuevoEstado = TextEditingController();
-    final orderProvider = Provider.of<OrderProvider>(context, listen: false);
-    orderProvider.getAllComandas();
+    final Size size = MediaQuery.of(context).size;
 
     return Scaffold(
       appBar: AppBar(
@@ -49,7 +38,6 @@ class _ChefScreenState extends State<ChefScreen> {
                         child: const Text("Aceptar"),
                         onPressed: () async {
                           await FirebaseAuth.instance.signOut();
-                          // ignore: use_build_context_synchronously
                           Navigator.pushNamedAndRemoveUntil(
                               context, 'Home', (route) => false);
                         },
@@ -62,47 +50,100 @@ class _ChefScreenState extends State<ChefScreen> {
           )
         ],
       ),
-      body: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: orderProvider.comandas.length,
-        itemBuilder: (context, index) {
-          Comanda comanda = orderProvider.comandas[index];
-          if (comanda.estado == "En preparación") {
-            return Container(
-              width: 200,
-              margin: const EdgeInsets.all(8),
-              child: Card(
-                elevation: 20,
-                child: Column(
-                  children: [
-                    Text('Comanda #${comanda.id}'),
-                    const SizedBox(height: 8),
-                    Text('Mesa: ${comanda.mesa}'),
-                    const SizedBox(height: 8),
-                    DropdownButton<String>(
-                      value: comanda.estado,
-                      onChanged: (nuevoEstado) {
-                        orderProvider.updateOrderState(
-                            comanda.mesa!, nuevoEstado!);
-                      },
-                      items: [
-                        'En preparación',
-                        'Listo para servir',
-                        'Entregado'
-                      ].map<DropdownMenuItem<String>>(
-                        (estado) {
-                          return DropdownMenuItem<String>(
-                            value: estado,
-                            child: Text(estado),
-                          );
-                        },
-                      ).toList(),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          }
+      body: Consumer<OrderProvider>(
+        builder: (context, orderProvider, _) {
+          orderProvider.getAllComandas();
+
+          return Wrap(
+            spacing: 5,
+            runSpacing: 5,
+            children: orderProvider.comandas
+                .where((comanda) => comanda.estado == "En preparación")
+                .map((comanda) => Container(
+                      width: size.width * 0.19,
+                      height: size.height * 0.4,
+                      margin: const EdgeInsets.all(5),
+                      child: Card(
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Comanda #${comanda.id}',
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                'Mesa: ${comanda.mesa}',
+                                style: const TextStyle(
+                                  fontStyle: FontStyle.italic,
+                                  fontSize: 14,
+                                ),
+                              ),
+                              const SizedBox(height: 12),
+                              Expanded(
+                                child: SingleChildScrollView(
+                                  child: ListView.builder(
+                                    shrinkWrap: true,
+                                    physics:
+                                        const NeverScrollableScrollPhysics(),
+                                    itemCount: comanda.productos!.length,
+                                    itemBuilder: (context, index) {
+                                      return ListTile(
+                                        title: Text(
+                                          comanda.productos![index].nombre ??
+                                              '',
+                                        ),
+                                        trailing: Text(comanda
+                                            .productos![index].cantidad
+                                            .toString()),
+                                        dense: true,
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 12),
+                              DropdownButton<String>(
+                                value: comanda.estado,
+                                onChanged: (nuevoEstado) {
+                                  orderProvider.updateOrderState(
+                                    comanda.mesa!,
+                                    nuevoEstado!,
+                                  );
+                                },
+                                items: ['En preparación', 'Entregado']
+                                    .map<DropdownMenuItem<String>>(
+                                  (estado) {
+                                    return DropdownMenuItem<String>(
+                                      value: estado,
+                                      child: Text(
+                                        estado,
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ).toList(),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ))
+                .toList(),
+          );
         },
       ),
     );
